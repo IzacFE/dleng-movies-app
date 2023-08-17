e70df8a18e518f249a725adf9b11b611047d5d81
IzacFE <izacaqsha@gmail.com>
2023-08-17 18:54:29 +0700

    All MVP Features are working, starting sprint

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,46 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.less'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   params$: any;
 
-  constructor(
-    private readonly router: Router,
-    private readonly rootRoute: ActivatedRoute
-  ) {
-    this.params$ = router.events.pipe(
-      filter((e) => e instanceof NavigationEnd),
-      map((e) => this.getParams(this.rootRoute))
-    );
-  }
-
-  ngOnInit(): void {
-    this.paramsSnapshot();
-  }
-
-  paramsSnapshot() {
-    console.log(this.getParams(this.rootRoute));
-
-    return this.getParams(this.rootRoute);
-  }
-
-  private getParams(route: ActivatedRoute) {
-    // route param names (eg /a/:personId) must be ditinct within
-    // a route otherwise they'll be overwritten
-    let params = route.url;
-    // params = { ...route.snapshot.queryParams, ...params };
-    // if (route.children) {
-    //   for (let r of route.children) {
-    //     params = { ...this.getParams(r), ...params };
-    //   }
-    // }
-    return params;
-  }
+  constructor() {}
 }
