24dcf4b130ae05ad5b5a07dc8ee1fca82eca9875
IzacFE <izacaqsha@gmail.com>
2023-08-18 19:59:06 +0700

    favorite button is done, notes there is bug within the icon color

---
+++ src/app/components/favorite-button/favorite-button.component.ts
@@ -0,0 +1,52 @@
+import { Component, Input } from '@angular/core';
+import { FavoriteStorageService } from 'src/app/service/favorite-storage.service';
+
+@Component({
+  selector: 'app-favorite-button',
+  templateUrl: './favorite-button.component.html',
+  styleUrls: ['./favorite-button.component.less'],
+})
+export class FavoriteButtonComponent {
+  @Input() data: any = {};
+
+  isFav: string = '';
+  favorites: any;
+  constructor(private favoriteService: FavoriteStorageService) {}
+  ngOnInit(): void {
+    this.favoriteService.getFavorites().subscribe((favorites) => {
+      this.favorites = favorites;
+    });
+  }
+
+  addFavorite(movie: any) {
+    this.favoriteService.addFavorite(movie);
+  }
+
+  deleteFavorite(movie: any) {
+    this.favoriteService.deleteFavorite(movie);
+  }
+
+  checkFavorite() {
+    const index = this.favorites.findIndex(
+      (item: any) => item.id === this.data.id
+    );
+    if (index > 0) {
+      this.isFav = 'favorite';
+    } else {
+      this.isFav = '';
+    }
+    console.log(this.isFav, 'fav');
+  }
+
+  onFavClick(movie: any): void {
+    const index = this.favorites.findIndex((item: any) => item.id === movie.id);
+
+    if (index < 0) {
+      this.addFavorite(movie);
+      this.isFav = '';
+    } else {
+      this.deleteFavorite(movie);
+      this.isFav = 'favorite';
+    }
+  }
+}
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { TmdbApiServiceService } from 'src/app/service/tmdb-api-service.service';
 
@@ -7,20 +7,26 @@ import { TmdbApiServiceService } from 'src/app/service/tmdb-api-service.service'
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.less'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchResult: any;
+  loading: boolean = false;
   constructor(private service: TmdbApiServiceService) {}
 
-  searchResult: any;
+  ngOnInit(): void {
+    // this.trendingData();
+  }
 
   searchForm = new FormGroup({
     searchName: new FormControl(null),
   });
 
   submitForm() {
+    this.loading = true;
     console.log(this.searchForm.value, 'searchForm');
     this.service.getSearchMovie(this.searchForm.value).subscribe((result) => {
       console.log(result, 'resultMovie');
       this.searchResult = result.results;
+      this.loading = false;
     });
   }
 }
