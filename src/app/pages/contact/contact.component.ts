3a9c01dc929e6af6e20dee46421f4b595d407346
IzacFE <izacaqsha@gmail.com>
2023-08-23 13:12:24 +0700

    Contact form Validation

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,42 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { ContactSheetApiService } from 'src/app/service/contact-sheet-api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.less'],
 })
-export class ContactComponent implements OnInit {
-  googleSheetForm!: FormGroup;
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private service: ContactSheetApiService
-  ) {
-    this.googleSheetForm = this.formBuilder.group({
-      name: formBuilder.control(''),
-      email: formBuilder.control(''),
-      phone: formBuilder.control(0),
-      message: formBuilder.control(''),
-    });
-  }
-
-  ngOnInit(): void {}
-
-  public onSubmit() {
-    const name = this.googleSheetForm.value.name;
-    const email = this.googleSheetForm.value.email;
-    const phone = this.googleSheetForm.value.phone;
-    const message = this.googleSheetForm.value.message;
-
-    this.service.createSheet(name, email, phone, message).subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
-  }
+export class ContactComponent {
+  constructor() {}
 }
